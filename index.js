00dc087e507d899ae75f4dcc4a5ede898561cfe8
Niayesh99 <n.tabrikk@gmail.com>
2024-12-21 17:25:08 +0100

    add math functions

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+
+function add(a, b) {
+    return a + b;
+  }
+  function substract(a, b) {
+    return a - b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function  divide(a, b) {
+    return a / b;
+  }
+const additionResult = add(2, 4);
+const substractionResult = substract(2, 4);
+const multiplicationResult = multiply(2, 4);
+const divisionResult =divide(2, 4);
+console.log(`Result of addition is :${additionResult}`);
+console.log(`Result of substract is :${substractionResult}`);
+console.log(`Result of multiply is :${multiplicationResult}`);
+console.log(`Result of divide is :${divisionResult}`);
