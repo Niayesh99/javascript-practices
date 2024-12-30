3d9c3f243c3241efc1b6d1cd813b2e2d7193875d
Niayesh99 <n.tabrikk@gmail.com>
2024-12-30 16:33:50 +0100

    create function to sort elements in array

--- array.js
+++ array.js
@@ -11,3 +11,15 @@ function getRandomName() {
 const result = getRandomName();
 console.log(result);
 
+function alphabeticSort(names) {
+    const sortedWords = names.sort();
+    console.log(sortedWords); 
+}
+alphabeticSort(names);
+function descendingSort(names){
+    const sortedWords = names.sort();
+    const descending =sortedWords.reverse();
+    console.log(descending); 
+}
+descendingSort(names);
+
