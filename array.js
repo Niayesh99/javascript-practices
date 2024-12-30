d6874d1a35942f2f47b1d850ba9966f615626590
Niayesh99 <n.tabrikk@gmail.com>
2024-12-30 16:46:20 +0100

    create add and remove functions for array elements

--- array.js
+++ array.js
@@ -12,14 +12,29 @@ const result = getRandomName();
 console.log(result);
 
 function alphabeticSort(names) {
-    const sortedWords = names.sort();
-    console.log(sortedWords); 
+  const sortedWords = names.sort();
+  console.log(sortedWords);
 }
 alphabeticSort(names);
-function descendingSort(names){
-    const sortedWords = names.sort();
-    const descending =sortedWords.reverse();
-    console.log(descending); 
+function descendingSort(names) {
+  const sortedWords = names.sort();
+  const descending = sortedWords.reverse();
+  console.log(descending);
 }
 descendingSort(names);
 
+function addElement(element) {
+  names.push(element);
+  console.log(names);
+}
+addElement("Rose");
+
+function removeValue(names, value) {
+  const index = names.indexOf(value);
+  if (index !== -1) {
+    names.splice(index, 1);
+  }
+  return names;
+}
+
+console.log(removeValue(names, "Mia"));
